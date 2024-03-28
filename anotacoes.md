dc01b0bb4f8637ddfd954fd2cb5a1059949c44ff
Gabriel Oliveira <gabriel_mrsonus@outlook.com>
2024-03-27 23:47:28 -0300

    fundamentos do JS

--- anotacoes.md
+++ anotacoes.md
@@ -44,3 +44,97 @@ const person ={
     console.log(firstColor)
     console.log(SecondColor)
 ```
+
+<b>• Funções</b>
+
+```js
+    function displayInfo({firstName, age}){
+            console.log(`${firstName} tem ${age} anos!`)    
+    }
+```
+
+<b>• Aplicação em React</b>
+
+```html
+    <h1>
+        Bem-vindo, {firstName}! Você tem {age} anos.
+    </h1>
+```
+
+<h1>Promises e Asyns/await</h1>
+
+<h2>• Promises</h2>
+<p>Uma promise é um objeto que representa a eventual conclusão (ou a falha) de uma operação assíncrona e seu valor resultante</p>
+
+<b>Exemplo:</b>
+
+```js
+    const promise = new Promise ((resolve, reject) => {
+        setTimeout(() => {
+            resolve("Dados Recebidos!");
+            reject("Erro ao buscar os dados!");
+        },2000)
+    })
+    
+    promise
+    .then(result => console.log(result))
+    .catch(error => console.log(error))
+```
+
+<h2>• Async/await</h2>
+
+<p>uma função async sempre retorna uma promise. Dentro de uma função async você pode usar await para esperar que uma promise seja resolvida</p>
+
+<b>Exemplo:</b>
+
+```js
+    async function fatchData(){
+        try{
+            let response = await fetch('https://jsonplaceholder.typicode.com/todos/1')
+            let data = await response.json()
+            console.log(data)
+        }catch(error){
+            console.error("Houve um erro doido:",error)
+        }
+    }
+
+    fatchData()
+```
+<br/>
+<h1>Map, Filter e Reduce</h1>
+
+<b>Map</b>
+
+```js
+ const numbers = [1,2,3,4,5]
+    const doubled = numbers.map(num => num*2)
+    console.log(doubled)
+```
+
+<b>Filter</b>
+
+```js
+    const numbers2 = [1,2,3,4,5]
+    const evens = numbers2.filter(num => num % 2 === 0)
+    console.log(evens)
+```
+
+<b>Reduce</b>
+
+```js
+    const numbers3 = [1,2,3,4,5]
+    const sum = numbers3.reduce((accumulator, current) => accumulator + current, 0)
+    console.log(sum)
+```
+
+<h3>Utilização no React<h3>
+
+```js
+    function TodoList({tasks}){
+        return(
+            <ul>
+                {tasks.map(task => <li key={task.id}>{task.name}</li>)}
+            </ul>
+        )
+    }
+```
--- src/jsbasico.jsx
+++ src/jsbasico.jsx
@@ -1,24 +1,103 @@
 export default function Jsbasico(){
 
-    const person ={
-        firstName: "João",
-        lastName: "Silva",
-        age: 30
-    };
 
-    const {firstName, lastName} = person
+    //Destructuring --------------------
 
-    const colors = ['vermelho','azul','verde']
+    // const person ={
+    //     firstName: "João",
+    //     lastName: "Silva",
+    //     age: 30
+    // };
 
-    const [firstColor, SecondColor] = colors
+    // const {firstName, lastName, age} = person
 
-    console.log(firstColor)
-    console.log(SecondColor)
+    // const colors = ['vermelho','azul','verde']
+
+    // const [firstColor, SecondColor] = colors
+
+//    function displayInfo({firstName, age}){
+//         console.log(`${firstName} tem ${age} anos!`)    
+//    }
+
+
+    // PROMISES ----------------------
+
+    // const promise = new Promise ((resolve, reject) => {
+    //     setTimeout(() => {
+    //         resolve("Dados Recebidos!");
+    //         reject("Erro ao buscar os dados!");
+    //     },2000)
+    // })
+    
+    // promise
+    // .then(result => console.log(result))
+    // .catch(error => console.log(error))
+
+    
+    //ASYNC/AWAIT --------------------
+
+    // async function fatchData(){
+    //     try{
+    //         let response = await fetch('https://jsonplaceholder.typicode.com/todos/1')
+    //         let data = await response.json()
+    //         console.log(data)
+    //     }catch(error){
+    //         console.error("Houve um erro doido:",error)
+    //     }
+    // }
+
+    // fatchData()
+
+
+    // // MAP ---------------------------------
+    
+    // const numbers = [1,2,3,4,5]
+    // const doubled = numbers.map(num => num*2)
+    // console.log(doubled)
+
+
+    // //FILTER -------------------------------
+
+    // const numbers2 = [1,2,3,4,5]
+    // const evens = numbers2.filter(num => num % 2 === 0)
+    // console.log(evens)
+
+
+    // //Reduce -------------------------------
+
+    // const numbers3 = [1,2,3,4,5]
+    // const sum = numbers3.reduce((accumulator, current) => accumulator + current, 0)
+    // console.log(sum)
+
+
+    //UTILIZAÇÃO NO REACT ----------------
+
+    // function TodoList({tasks}){
+    //     return(
+    //         <ul>
+    //             {tasks.map(task => <li key={task.id}>{task.name}</li>)}
+    //         </ul>
+    //     )
+    // }
+
+    
 
     return(
         <>
-            <h1>{firstName} {lastName} {person.age}</h1>
-            <h2>{firstColor}, {SecondColor}</h2>
+
+            {/* Destructuring -----------------------------*/}
+
+            {/* <h1>{firstName} {lastName} {person.age}</h1>
+            <h2>{firstColor}, {SecondColor}</h2> */}
+
+            {/* {displayInfo(person)} */}
+
+            {/* <h1>
+                Bem-vindo, {firstName}! Você tem {age} anos.
+            </h1> */}
+
+            {/* {TodoList} */}
         </>
     )
 } 
+
